e4368c00f8d3dce426a41b2cc4e54162344cfe5f
ramon <ramonltorres@gmail.com>
2015-11-06 17:31:16 +0000

    resume and styling of samples

--- js/rah.js
+++ js/rah.js
@@ -6,100 +6,29 @@
 	return Math.floor((Math.random() * max) + min);
     }
     
-    function cloudPass() {
-	$.each($('.cloud'), function (k,el) {
-	    var elLeft = $(el).position().left;
-	    var elTop = $(el).position().top;
-	    var pos = $(window).width();
-	    var speed = $(el).attr('class').split(' ')[1].charAt(1) * .2;
-	    if(elLeft < -500) {
-		elLeft = pos;
-		elTop = getRandomNumber($(window).height()/2);
-	    }
-
-	    $(el).css({
-		'left': elLeft - 1*speed + 'px',
-		'top': elTop + 'px'
-	    });
-
-
-	    console.log($('.cloud').length);
-	    if($('.cloud').length == null || k == $('.cloud').length) {
-
-	    }
-	});
-	window.requestAnimationFrame(cloudPass);
-    }
-
     var maxClouds = 10;
     var count = 0;
 
-    function setClouds() {
-
-	function cloud() {
-	    var num = getRandomNumber(maxClouds);
-	    var idx = getRandomNumber(4);
-	    var elem = '<div class="cloud x' + idx + ' cloud' + count + '"></div>';
-
-	    setTimeout(function () {
-		$('.landscape').find('.clouds').append(elem);
-		$('.cloud'+count).css({
-		    'left': $(window).width() + 'px',
-		    'top' : getRandomNumber($(window).height()/4) + 'px'
-		});
-		
-		if(count <=10) {
-		    count++;
-		    cloud();
-		}
-	    },getRandomNumber(6000, 3000));
-	}
-
-	cloud();
-    }
-    
     function setNavBar () {
 	
     }
 
     function setParallax () {
-	var delay = $(window).scrollTop() * .4;
-	$('.landscape').css({
-	    'top': '-' + delay + 'px'
-	});
+
     }
 
     function renderLandscape () {
-	setClouds();
-	cloudPass();
-	setNavBar();
-	setParallax();
+
     }
 
     function resize () {
 	$('.landscape').height($(window).height());
-	$('.content').css({'margin-top':$(window).height()+'px'});
-    }
-
-    function moveCity () {
-	var el = $('.city');
-	el.css({
-	    'left': '-' + $(window).scrollTop() + 'px'
-	});
-    }
-
-    function moveBeach() {
-	var el = $('.beach');
-	el.css({
-	    'right': '-' + $(window).scrollTop() + 'px'
-	});
+	//$('.content').css({'margin-top':$(window).height()+'px'});
     }
 
     $(window).scroll(function () {
 	if($(window).scrollTop() <= $(window).height()) {
-	    moveCity();
-	    moveBeach();
-	    setParallax();
+
 	}
     });
 
